035803ab6debdcbf7632c0298d555132a0fce0f8
Kevin Kee <kevinkee9@gmail.com>
2020-06-24 16:47:19 +0900

    WIP Functions which change obj props moved to class methods
    Any functions which pertained to changing instantiated objects are now
    class methods. This makes the code (relatively) easier to follow. The
    goal is to get everything that directly changes objects into the classes
    and use etiquette.py to handle just the logic.
    
    Added extensive comments to etiquette.py, as it still is not clear from
    the code what is being done. (Fix this before merge to master.)

--- etiquette.py
+++ etiquette.py
@@ -2,57 +2,98 @@ from modules.classes import UrinalSession, StallRow, Stall
 import time
 
 
-def run_cycles(session, stall_row, stalls):
-    for cycle in range(session.total_duration):
-        stall_row.occupancy = get_occupied_stalls(stalls)
-
-        if len(stall_row.occupancy) >= session.stall_count:
-            if cycle % session.cycle_interval == 0:
-                stall_row.queued += 1
-            continue
-
-        if cycle % session.cycle_interval == 0 or stall_row.queued > 0:
-            process_cycle(stall_row.occupancy, session.cycle_duration, stalls)
-            if stall_row.queued > 0:
-                stall_row.queued -= 1
-            time.sleep(1/5)
+def get_lowest_exposure(occ, st):
+    vacant = [i for j, i in enumerate(st) if j not in occ]
+    if len(occ) % 2 == 0:
+        vacant.reverse()
+    lowest_exposure = sorted(
+        vacant, key=lambda stall: stall.exposure, reverse=False
+    )[0]
+    return lowest_exposure
 
 
-def get_occupied_stalls(stalls):
-    occupied = []
-    for i, stall in enumerate(stalls):
-        if int(stall.set_time()) > 0:
-            occupied.append(i)
-    return occupied
-
-
-def process_cycle(occupied, cycle_duration, stalls):
-    def add_occupant():
-        vacant = [i for j, i in enumerate(stalls) if j not in occupied]
-        if len(occupied) % 2 == 0:
-            vacant.reverse()
-        lowest_exposure = sorted(
-            vacant, key=lambda stall: stall.exposure, reverse=False
-        )[0]
-        lowest_exposure.time = cycle_duration
-
-    def process_stalls():
-        for stall in stalls:
-            stall.set_exposure(cycle_duration, stalls)
-
-    def print_stalls():
-        for stall in stalls:
-            icon = 'x' if stall.time > 0 else 'o'
-            print(f'|{icon}|', end='')
-        print('\r')
-
-    add_occupant()
-    process_stalls()
-    print_stalls()
+def print_stalls(st):
+    for stall in st:
+        icon = 'x' if stall.time > 0 else 'o'
+        print(f'|{icon}|', end='')
+    print('\r')
 
 
 if __name__ == '__main__':
-    session_obj = UrinalSession()
-    stall_row_obj = StallRow()
-    stalls_obj = [Stall(stall) for stall in range(session_obj.stall_count)]
-    run_cycles(session_obj, stall_row_obj, stalls_obj)
+    session = UrinalSession(
+        cd=int(input("Enter cycle duration: ")),
+        cc=int(input("Enter number of cycles to run: ")),
+        ci=int(input("Enter interval between cycles: ")),
+        sc=int(input("Enter number of stalls: "))
+    )
+    stall_row = StallRow()
+    stalls = [Stall(stall) for stall in range(session.stall_count)]
+
+    for cycle in range(session.total_duration):
+        interval = cycle % session.cycle_interval == 0
+
+        """
+        Check if someone is leaving, update stall occupancy. We do this first
+        because want to allow someone to leave a stall and newcomer to enter
+        it in same cycle.
+        """
+        stall_row.update_occupancy(stalls)
+
+        """
+        Decrement the time of every occupied stall by one.
+        """
+        for stall in stalls:
+            stall.decrememt_time()
+
+        """
+        If occupancy is at maximum, check interval and add to queue if needed.
+        """
+        stall_row.incrememt_queue(
+            interval, stall_row.occupancy, session.stall_count
+        )
+
+        """
+        Do not continue current loop if there are no vacant stalls.
+        """
+        if stall_row.queued > 0:
+            continue
+
+        """
+        The below block of code applies at an interval (someone enters the
+        room) or at a cycle when the queue is >0 (someone is already there,
+        waiting for a vacant stall).
+        """
+        if interval or stall_row.queued > 0:
+            """
+            Stall exposure should be calculated before assigning a stall, as
+            if someone left during the same cycle this would affect exposure.
+            """
+            for stall in stalls:
+                stall.set_exposure(session.cycle_duration, stalls)
+
+            """
+            Determine which stall has the lowest exposure and assign it to the
+            newcomer.
+            TODO: This is not actually working. Exposure is not being properly
+            calculated. The reverse bit might be throwing things off, in which
+            case it isn't so bad because that is just placeholder logic.
+            """
+            low_exp_stall = get_lowest_exposure(stall_row.occupancy, stalls)
+            low_exp_stall.add_occupant(session.cycle_duration)
+
+            """
+            Print stalls to console. This only happens when someone enters a
+            stall. Nothing is printed when someone exits a stall.
+            """
+            print_stalls(stalls)
+
+            """
+            Decrement queue if someone in queue and just took a stall.
+            """
+            stall_row.decrememt_queue()
+
+            """
+            Make the console output easier on the eyes before printing next
+            change in occupancy.
+            """
+            time.sleep(1/5)
--- modules/classes.py
+++ modules/classes.py
@@ -1,10 +1,10 @@
 class UrinalSession:
-    def __init__(self):
-        self.cycle_duration = int(input("Enter cycle duration: "))
-        self.cycle_count = int(input("Enter number of cycles to run: "))
-        self.cycle_interval = int(input("Enter interval between cycles: "))
-        self.stall_count = int(input("Enter number of stalls: "))
-        self.total_duration = self.cycle_duration * self.cycle_count + 1
+    def __init__(self, cd, cc, ci, sc):
+        self.cycle_duration = cd
+        self.cycle_count = cc
+        self.cycle_interval = ci
+        self.stall_count = sc
+        self.total_duration = cd * cc
 
 
 class StallRow:
@@ -12,16 +12,27 @@ class StallRow:
         self.queued = 0
         self.occupancy = []
 
+    def update_occupancy(self, stalls):
+        occupied = []
+        for i, stall in enumerate(stalls):
+            if int(stall.time) > 0:
+                occupied.append(i)
+        self.occupancy = occupied
+
+    def incrememt_queue(self, interval, occupancy, count):
+        if interval and len(occupancy) >= count:
+            self.queued += 1
+
+    def decrememt_queue(self):
+        if self.queued > 0:
+            self.queued -= 1
+
 
 class Stall:
     def __init__(self, stall):
         self.position = stall
         self.exposure = 0
         self.time = 0
-
-    def __str__(self):
-        return \
-            f"[{self.position}] exposure: {self.exposure}, time: {self.time}"
 
     def set_exposure(self, cycle_duration, stalls):
         def threat(neighbour):
@@ -35,8 +46,10 @@ class Stall:
             exposure += threat(stalls[i+1])
         self.exposure = exposure
 
-    def set_time(self):
+    def decrememt_time(self):
         time = int(self.time)
         if time > 0:
             self.time = time - 1
-        return self.time
+
+    def add_occupant(self, duration):
+        self.time = duration
